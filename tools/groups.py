3e9d0b119bee40b3d8603a8ce06b6715e50ba6eb
Tim Schubert <tim.schubert@tu-bs.de>
2018-09-21 15:43:01 +0200

    Add tool for fetching all student projects by group

---
+++ tools/groups.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+from sys import argv
+from subprocess import run
+from os import chdir
+from os.path import isdir
+
+course_url = argv[1]
+
+with open(argv[2], 'r', encoding="latin-1") as csv:
+    for line in csv:
+        tokens = line.split(';')
+        student = tokens[5].replace('"', "").split(" ")[0]
+        group = tokens[0].replace('"', "").split(" ")[0]
+        url = course_url + "/solutions/" + student + "/solutions"
+        path = "solutions/" + group + "/" + student
+        if isdir(path):
+            chdir(path)
+            run(["git", "pull"])
+            chdir("../../..")
+        else:
+            run(["git", "clone", url, path])
