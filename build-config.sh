946dbd22e61f7f1c296cb698a90227368ebb7fab
Tim Schubert <tim.schubert@tu-bs.de>
2018-09-21 12:46:11 +0200

    Build config using bash script

--- build-config.sh
+++ build-config.sh
@@ -1,7 +1,12 @@
+#!/bin/sh
+
+cat > python-gitlab.cfg <<EOF
 [global]
-default = default 
+default = default
 ssl_verify = true
 
 [default]
 url = $(echo ${CI_PROJECT_URL} | cut -d '/' -f -3)
 api_version = 4
+private_token = ${PRIVATE_API_TOKEN}
+EOF
